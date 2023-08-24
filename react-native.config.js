9804b2c053fffa4b987f5afc2664becdccf5b2d5
khai <mhdkhai03@gmail.com>
2023-08-24 23:07:14 +0700

    feat: assset

--- react-native.config.js
+++ react-native.config.js
@@ -3,7 +3,9 @@ module.exports = {
     ios: {},
     android: {},
   },
-  assets: ['./src/assets/fonts'],
+  assets: ['./src/assets/fonts', './src/font'],
+  iosAssets: ['./src/font/ios'],
+  androidAssets: ['./src/font/android'],
   dependencies: {
     'react-native-vector-icons': {
       platforms: {
