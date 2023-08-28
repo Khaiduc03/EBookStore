9c527ca5be437b0ee46d37cc5ff76892906d8396
Kane12703 <117529368+Kane12703@users.noreply.github.com>
2023-08-28 18:03:44 +0700

    add-theme

--- src/screens/main/home/Home/styles.ts
+++ src/screens/main/home/Home/styles.ts
@@ -1,10 +1,19 @@
-
-import { makeStyles } from '@rneui/themed';
+import {StyleSheet} from 'react-native';
+import {fontFamilySetup} from '../../../../utils/font';
+import {makeStyles} from '@rneui/themed';
 
 const useStyles = makeStyles(({colors}) => ({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  text: {
+    fontSize: 26,
+    fontFamily: fontFamilySetup.bold,
+    color: colors.divider,
+  },
+  textTopics: {
+    color: colors.black,
   },
 }));
 
