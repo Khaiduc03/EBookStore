bc66561edb182ca80dcebf50eb94ea6fa2827ec7
HuyMatec <tranhoanghuy02.11.2003@gmail.com>
2023-11-19 19:07:10 +0700

    update data expole

--- src/screens/main/explore/Explore/index.tsx
+++ src/screens/main/explore/Explore/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { HeaderCustom } from '../../../../components';
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {HeaderCustom} from '../../../../components';
 import CarouselSquareList from './Components/CarouselSquareList';
 import ListHotComic from './Components/ListHotComic';
 import RecommendedSeries from './Components/RecommendedSeries';
@@ -10,37 +10,37 @@ import useStyles from './styles';
 
 const data = [
   {
-    title: 'Cô sáu mười ngón',
+    title: 'Exemplary wife',
     category: 'Fantasy',
     image:
       'https://img.dtruyen.com/public/images/large/923/kieuthecokhonggiandayconlamgiaugsyQgwG.jpg',
   },
   {
-    title: 'Cô chín mặt mu',
+    title: 'Porcelain flower girl',
     category: 'Drama',
     image:
       'https://img.dtruyen.com/public/images/large/1023/trove80trothanhnuphubancondoiluongthucuWo1Sse.jpg',
   },
   {
-    title: 'Cô mười ngự cu',
+    title: 'The eyes of a lover',
     category: 'Action',
     image:
       'https://img.dtruyen.com/public/images/large/thapnien70mynhanmemmairuLCZ6p.jpg',
   },
   {
-    title: 'Cô hai chai mặt',
+    title: 'Country girl',
     category: 'Comedy',
     image:
       'https://img.dtruyen.com/public/images/large/thapnien90taohuonggiangxemqueCLnJfWG.jpg',
   },
   {
-    title: 'Cô ba la ma to',
+    title: 'Young lady saves money to travel',
     category: 'Mystery',
     image:
       'https://img.dtruyen.com/public/images/large/923/trongsinhvethoinguyenthuylamruongZ2bsyDe.jpg',
   },
   {
-    title: 'Cô cô nhiều chuyện',
+    title: 'Girl selling chrysanthemums',
     category: 'Horror',
     image:
       'https://img.dtruyen.com/public/images/large/823/quaphunhanongnuoiconlamgiauDk5ys24.jpg',
