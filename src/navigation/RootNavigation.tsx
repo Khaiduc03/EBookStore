78575f8e3ea6bb2156786247854e047b7c607e87
hqDat <128505445+Dat-03@users.noreply.github.com>
2023-11-20 17:20:54 +0700

    feat: User pick rating star

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -41,7 +41,7 @@ const RootNavigation = () => {
         barStyle={themeMode}
       />
       {/* {enableSignIn ? <AppNavigator /> : <AuthNavigator />} */}
-      <RatingComicScreen />
+      <AppNavigator />
     </NavigationContainer>
   );
 };
